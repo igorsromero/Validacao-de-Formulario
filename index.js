b3d0b8cf555940d6e17295f7297455a2c17765d5
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-05-18 14:21:07 -0300

    Validação completa com Cookie-Parser.

--- index.js
+++ index.js
@@ -22,7 +22,21 @@ app.use(session({
 app.use(flash());
 
 app.get("/", (req, res) => {
-    res.render("./index");
+
+    var emailError = req.flash("emailError");
+    var pontosError = req.flash("pontosError");
+    var nomeError = req.flash("nomeError");
+    var email = req.flash("email");
+    var nome = req.flash("nome");
+    var pontos = req.flash("pontos");
+    emailError = (emailError == undefined || emailError.length == 0) ? undefined : emailError;
+    pontosError = (pontosError == undefined || pontosError.length == 0) ? undefined : pontosError;
+    nomeError = (nomeError == undefined || nomeError.length == 0) ? undefined : nomeError;
+    email = (email == undefined || email.length == 0) ? "" : email;
+    nome = (nome == undefined || nome.length == 0) ? "" : nome;
+    pontos = (pontos == undefined || pontos.length == 0) ? "" : pontos;
+
+    res.render("./index", { emailError, pontosError, nomeError, email: email, nome: nome, pontos: pontos });
 });
 
 app.post('/form', function (req, res) {
@@ -31,23 +45,31 @@ app.post('/form', function (req, res) {
     var pontosError;
     var nomeError;
 
-    if(email == undefined || email == ""){
+    if (email == undefined || email == "") {
         emailError = "O e-mail não pode ser vazio.";
     }
 
-    if(pontos == undefined || pontos < 20){
+    if (pontos == undefined || pontos < 20) {
         pontosError = "Você não pode ter menos de 20 pontos.";
     }
 
-    if(nome == undefined || nome == ""){
+    if (nome == undefined || nome == "") {
         nomeError = "O nome não pode ser vazio.";
     }
 
-    if(nome.length < 4){
+    if (nome.length < 4) {
         nomeError = "O nome é muito pequeno.";
     }
 
-    if(emailError != undefined || pontosError != undefined || nomeError != undefined){
+    if (emailError != undefined || pontosError != undefined || nomeError != undefined) {
+        req.flash("emailError", emailError);
+        req.flash("pontosError", pontosError);
+        req.flash("nomeError", nomeError);
+
+        req.flash("email", email);
+        req.flash("nome", nome);
+        req.flash("pontos", pontos);
+
         res.redirect("/");
     } else {
         res.send("Não há erros.");
